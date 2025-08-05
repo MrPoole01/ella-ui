63a36b6f156836d2379d3b6b7ad12026030cb84c
rick <recardo.poole@gmail.com>
2025-08-05 12:40:46 -0600

    Added a new Ella Theme ane the Manage Saved Work flyour

--- src/components/features/TemplateDrawer.jsx
+++ src/components/features/TemplateDrawer.jsx
@@ -375,17 +375,17 @@ const TemplateDrawer = ({ isOpen, onClose }) => {
           {/* Header Tabs */}
           <div className="template-drawer__header">
             <div className="template-drawer__tabs">
-            <button 
-                className={`template-drawer__tab ${activeTab === 'ella-templates' ? 'template-drawer__tab--active' : ''}`}
-                onClick={() => setActiveTab('ella-templates')}
-              >
-                Ella Templates
-              </button>
               <button 
                 className={`template-drawer__tab ${activeTab === 'special-additions' ? 'template-drawer__tab--active' : ''}`}
                 onClick={() => setActiveTab('special-additions')}
               >
                 Special Edition Templates
+              </button>
+              <button 
+                className={`template-drawer__tab ${activeTab === 'ella-templates' ? 'template-drawer__tab--active' : ''}`}
+                onClick={() => setActiveTab('ella-templates')}
+              >
+                All Templates
               </button>
               <button 
                 className={`template-drawer__tab ${activeTab === 'saved-templates' ? 'template-drawer__tab--active' : ''}`}
